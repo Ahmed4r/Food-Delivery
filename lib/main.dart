d66b953b7eb1a4bfb1a16b4e9e0d5ac33241977c
hegazy <ahmedrady03@gmail.com>
2025-07-24 19:22:33 +0300

    add localizations

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/screens/customer/edit_profile.dart';
 import 'package:food_delivery/screens/customer/homepage.dart';
+import 'package:food_delivery/screens/drawer/porfile.dart';
+import 'package:food_delivery/screens/drawer/settings_page.dart';
 import 'package:food_delivery/screens/location/location_page.dart';
 import 'package:food_delivery/screens/login/forgot_password/forgot_password.dart';
 import 'package:food_delivery/screens/login/login.dart';
@@ -32,20 +35,29 @@ class FoodDelivery extends ConsumerWidget {
     final themeMode = ref.watch(themeModeProvider);
     return ScreenUtilInit(
       designSize: Size(375, 812),
+      
+      
       child: MaterialApp(
+        locale: context.locale,
+        supportedLocales: context.supportedLocales,
+        localizationsDelegates: context.localizationDelegates,
         debugShowCheckedModeBanner: false,
         initialRoute: LoginPage.routeName,
-        routes: {LoginPage.routeName: (context) => LoginPage(),
+        routes: {
+     
+        LoginPage.routeName: (context) => LoginPage(),
         ForgotPassword.routeName: (context) => ForgotPassword(),
         OtpScreen.routeName: (context) => OtpScreen(),
         SignupPage.routeName: (context) => SignupPage(),
         LocationAccessPage.routeName: (context) => LocationAccessPage(),
         Homepage.routeName: (context) => Homepage(),
-        
+        ProfileScreen.routeName:  (context) => ProfileScreen(),
+        EditProfile.routeName: (context) => EditProfile(),
+        SettingsPage.routeName: (context) => SettingsPage(),
         },
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.system,
+        themeMode: themeMode,
       ),
     );
   }
