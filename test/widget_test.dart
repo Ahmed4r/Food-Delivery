087bc9bbbe0d507b09a7230736c9cd5e449112d4
hegazy <ahmedrady03@gmail.com>
2025-07-26 01:01:13 +0300

    edit test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:food_delivery/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame
-    await tester.pumpWidget(const FoodDelivery());
+    await tester.pumpWidget(const FoodDelivery(token: false,flag: false,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
