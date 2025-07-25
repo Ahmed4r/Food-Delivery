6d5d6b063cb088296c8182075ae90457cfa70d33
hegazy <ahmedrady03@gmail.com>
2025-07-26 01:01:31 +0300

    add on boarding

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,17 +1,12 @@
+
+import 'dart:developer';
+
 import 'package:animated_introduction/animated_introduction.dart';
 import 'package:easy_localization/easy_localization.dart';
-// Onboarding Screen
-//
-// Description:
-//   Provides an animated onboarding experience for the food delivery app.
-//   Introduces users to key features and guides them to the login screen.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_delivery/screens/login/login.dart';
 import 'package:food_delivery/theme/app_colors.dart';
-import 'package:food_delivery/theme/theme_provider.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OnboardingScreen extends StatelessWidget {
   static const routeName = '/onboarding-screen';
@@ -20,17 +15,21 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
-    return AnimatedIntroduction(
-      
+ 
+    return AnimatedIntroduction(   
       activeDotColor: isDark ? AppColors.white : Colors.black,
       containerBg:isDark ? AppColors.secondary : AppColors.white, //background color
       textColor: isDark ?AppColors.white : Colors.white, //text color : lightmode
-      footerBgColor: isDark ? AppColors.black :Colors.orange, //footer container background color
+      footerBgColor: isDark ? AppColors.black :Colors.deepOrange, //footer container background color
       inactiveDotColor:isDark ? AppColors.primary : Colors.white,
       slides: pages,
       indicatorType: IndicatorType.circle,
-      onDone: () {
+      onDone: ()async {
         Navigator.of(context).pushReplacementNamed(LoginPage.routeName);
+        
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+await prefs.setBool('hasSeenOnboarding', true);
+log('onboarding screen flaged${prefs.getBool('hasSeenOnboarding').toString()}');
       },
     );
     
@@ -39,6 +38,7 @@ class OnboardingScreen extends StatelessWidget {
    List<SingleIntroScreen> pages = [
   
    SingleIntroScreen(
+
     title:  'onboarding_title_1'.tr(),
     description:'onboarding_desc_1'.tr(),
     imageAsset: 'assets/images/onboarding1.png',
