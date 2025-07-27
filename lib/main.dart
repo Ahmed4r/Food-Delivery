2ec62f9c5ee603371283be3346cb671531c0f807
hegazy <ahmedrady03@gmail.com>
2025-07-28 01:43:10 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'dart:developer';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_delivery/screens/customer/edit_profile.dart';
-import 'package:food_delivery/screens/customer/homepage.dart';
+import 'package:food_delivery/screens/customer/customer_homepage.dart';
+import 'package:food_delivery/screens/customer/restaurant_view.dart';
 import 'package:food_delivery/screens/drawer/address.dart';
 import 'package:food_delivery/screens/drawer/faqs.dart';
+import 'package:food_delivery/screens/drawer/payments.dart';
 import 'package:food_delivery/screens/drawer/personal_info.dart';
 import 'package:food_delivery/screens/drawer/settings_page.dart';
 import 'package:food_delivery/screens/location/get_geolocation.dart';
@@ -15,15 +18,30 @@ import 'package:food_delivery/screens/login/forgot_password/forgot_password.dart
 import 'package:food_delivery/screens/login/login.dart';
 import 'package:food_delivery/screens/login/otp/otp_screen.dart';
 import 'package:food_delivery/screens/onboarding_screen.dart';
+import 'package:food_delivery/screens/restaurant/owner_homepage.dart';
 import 'package:food_delivery/screens/signup/signup.dart';
 import 'package:food_delivery/theme/theme.dart';
 import 'package:food_delivery/theme/theme_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'dart:developer' as developer;
 void main() async {
-
-  
+  debugPrint = (String? message, {int? wrapWidth}) {
+    developer.log(message ?? '', name: 'PaymobDebug');
+  };
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyBTKQUPtXgfbnQpIos4VoddRBmdMsSukzs",
+      appId: "1:4958439163:android:21cbb7fc4a5e0c5be3125e",
+      projectId: "foodapp-e68fd",
+      messagingSenderId: "4958439163",
+      storageBucket:  "foodapp-e68fd.firebasestorage.app"
+
+
+    )
+
+    
+);
   await EasyLocalization.ensureInitialized();
   await SharedPreferences.getInstance();
   var prefs = await SharedPreferences.getInstance();
@@ -32,6 +50,7 @@ void main() async {
   log(token.toString());
   log(flag.toString());
   runApp(
+    
     
     EasyLocalization(
 
@@ -87,6 +106,9 @@ class FoodDelivery extends ConsumerWidget {
         FaqsScreen.routeName: (context) => FaqsScreen(),
         AddressListScreen.routeName: (context) => AddressListScreen(),
         GetGeolocation.routeName: (context) => GetGeolocation(),
+        PaymentSystemScreen.routeName: (context) => PaymentSystemScreen(),
+        OwnerHomepage.routeName : (context)=> OwnerHomepage() ,
+        RestaurantViewScreen.routeName : (context)=> RestaurantViewScreen(),
         },
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
