dbdb4684d6fd4b8bcc0bc6c4d40f1ba76bcf1832
hegazy <ahmedrady03@gmail.com>
2025-07-28 23:02:02 +0300

    fix

--- lib/screens/restaurant/owner_homepage.dart
+++ lib/screens/restaurant/owner_homepage.dart
@@ -2,11 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:food_delivery/screens/restaurant/res_drawer.dart';
 import 'package:food_delivery/theme/app_colors.dart';
-import 'package:food_delivery/widgets/bottom_nav.dart';
-import 'package:food_delivery/widgets/custom_drawer.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
